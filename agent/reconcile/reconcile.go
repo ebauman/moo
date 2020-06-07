7ff19137543a61ea6c2630478450569465c15539
Eamon Bauman <eamon@eamonbauman.com>
2020-06-06 20:17:54 -0500

    fixed #1
    Signed-off-by: Eamon Bauman <eamon@eamonbauman.com>

--- agent/reconcile/reconcile.go
+++ agent/reconcile/reconcile.go
@@ -4,93 +4,71 @@ import (
 	"github.com/ebauman/moo/pkg/config"
 	"github.com/ebauman/moo/pkg/kubernetes"
 	"github.com/ebauman/moo/pkg/rancher"
-	"time"
 )
 
 type RegistrationStatus string
+
 const (
 	Unregistered RegistrationStatus = "Unregistered"
-	Registered RegistrationStatus = "Registered"
-	Unknown RegistrationStatus = "Unknown"
-
-	ErrorSleepDuration = 60
-	SuccessSleepDuration = 60
+	Registered   RegistrationStatus = "Registered"
 )
 
 func Reconcile(config *config.Config) {
-	config.Log.Debugf("starting reconciliation loop")
-	var registration_status RegistrationStatus
+	config.Log.Debugf("starting cluster registration")
+	var registrationStatus RegistrationStatus
 
-	registration_status = Unknown // initially
+	// start by checking registration status
+	registrationStatus = checkRegistration(config)
 
-	for {
-		// start by checking registration status
-		registration_status = check_registration(config)
-
-		if registration_status == Registered {
-			config.Log.Debugf("cluster already registered")
-			time.Sleep(time.Second * SuccessSleepDuration)
-			continue // sleep for 5 minutes, then reconcile again
-		}
-
-		if registration_status == Unknown {
-			continue // theoretically not possible, but you never know
-		}
-
-		config.Log.Infof("registering cluster with rancher %s and name %s", config.URL, config.ClusterName)
-
-		// unregistered at this point, which is where the work needs to be done.
-
-		// once we have a rancher client, check for existence of this cluster
-		cluster, err := rancher.CheckForCluster(config)
-		if err != nil {
-			config.Log.Errorf("error checking for cluster: %v", err)
-			time.Sleep(time.Second * ErrorSleepDuration)
-			continue
-		}
-
-		if cluster != nil && !config.UseExisting {
-			config.Log.Errorf("cluster %s already exists in rancher and --use-existing-cluster not passed", config.ClusterName)
-			time.Sleep(time.Second * ErrorSleepDuration)
-			continue
-		}
-
-		if cluster == nil {
-			// need to create cluster
-			cluster, err = rancher.RegisterCluster(config)
-			if err != nil {
-				config.Log.Errorf("error registering cluster with rancher: %v", err)
-				time.Sleep(time.Second * ErrorSleepDuration)
-				continue
-			}
-		}
-
-		// at this point we should have a valid cluster either by creation or config.UseExisting = true
-
-		// grab the manifests
-		manifest, err := rancher.GetYAMLManifestForCluster(cluster, config)
-		if err != nil {
-			config.Log.Errorf("error getting manifest from rancher: %v", err)
-			time.Sleep(time.Second * ErrorSleepDuration)
-			continue
-		}
-
-		// with this manifest, apply to local cluster
-		if err := kubernetes.ApplyManifest(config, manifest); err != nil {
-			config.Log.Errorf("error applying rancher import manifest: %v", err)
-			time.Sleep(time.Second * ErrorSleepDuration)
-			continue
-		}
-
-		config.Log.Info("cluster registered successfully")
-
-		// if all is successful at this point, then we have theoretically registered the cluster
-		// so let's sleep and loop again
-		time.Sleep(time.Second * SuccessSleepDuration)
+	if registrationStatus == Registered {
+		config.Log.Debugf("cluster already registered")
+		return // sleep for 5 minutes, then reconcile again
 	}
+
+	config.Log.Infof("registering cluster with rancher %s and name %s", config.URL, config.ClusterName)
+
+	// unregistered at this point, which is where the work needs to be done.
+
+	// once we have a rancher client, check for existence of this cluster
+	cluster, err := rancher.CheckForCluster(config)
+	if err != nil {
+		config.Log.Errorf("error checking for cluster: %v", err)
+		return
+	}
+
+	if cluster != nil && !config.UseExisting {
+		config.Log.Errorf("cluster %s already exists in rancher and --use-existing-cluster not passed", config.ClusterName)
+		return
+	}
+
+	if cluster == nil {
+		// need to create cluster
+		cluster, err = rancher.RegisterCluster(config)
+		if err != nil {
+			config.Log.Errorf("error registering cluster with rancher: %v", err)
+			return
+		}
+	}
+
+	// at this point we should have a valid cluster either by creation or config.UseExisting = true
+
+	// grab the manifests
+	manifest, err := rancher.GetYAMLManifestForCluster(cluster, config)
+	if err != nil {
+		config.Log.Errorf("error getting manifest from rancher: %v", err)
+		return
+	}
+
+	// with this manifest, apply to local cluster
+	if err := kubernetes.ApplyManifest(config, manifest); err != nil {
+		config.Log.Errorf("error applying rancher import manifest: %v", err)
+		return
+	}
+
+	config.Log.Info("cluster registered successfully")
 }
 
-func check_registration(config *config.Config) RegistrationStatus {
+func checkRegistration(config *config.Config) RegistrationStatus {
 	config.Log.Debugf("checking cluster registration status")
 	// check if our cluster is registered or not.
 	// registration is achieved when three things occur:
@@ -114,5 +92,5 @@ func check_registration(config *config.Config) RegistrationStatus {
 		return Unregistered
 	}
 
-	return Registered  // everything passed our checks
+	return Registered // everything passed our checks
 }
